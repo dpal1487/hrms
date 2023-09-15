4a1da0b1dc694b0493a4b8fd05a07e789aec1fb2
dpal1487 <dpal1487@gmail.com>
2023-09-15 11:00:35 +0530

    nothing

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -19,11 +19,5 @@ class Authenticate extends Middleware
         }
     }
 
-    protected function unauthenticated($request, array $guards)
-    {
-        abort(response()->json([
-            'api_status' => '401',
-            'message' => 'Unauthenticated',
-        ], 401));
-    }
+    
 }
